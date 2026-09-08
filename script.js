d9fbafaaedca325b0803abca7f11decd620e1146
DAVI ANTONINO NUNES DA SILVA <118570287+dansfisica85@users.noreply.github.com>
2026-09-08 20:51:50 -0300

    Improve feedback message with more user inputs
    Updated user feedback message to include additional preferences and details.

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ evento.preventDefault();
 
 const nome = document.querySelector("#nome").value;
 const genero = document.querySelector("#genero").value;
+const audio = document.querySelector("#audio").value;
+const filme = document.querySelector("#filme").value;
+const filmeserie = document.querySelector("#filmeserie").value;
+const canaisapp = document.querySelector("#canaisapp").value;
 
-resultado.textContent = Obrigado, ${nome}! Voce prefere filmes de ${genero}.;formulario.reset ();
-})
+resultado.textContent = Obrigado, ${nome}! Voce ve sempre ${filmeserie}, voce prefere filmes/series de ${genero}. Seu canal/aplicativo favorito é ${canaisapp} e seu filme favorito é ${filme}, idioma será ${audio}.;formulario.reset ();
+});
+
+
